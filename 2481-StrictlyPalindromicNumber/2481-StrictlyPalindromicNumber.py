98cebf03184a9ea16c86a76a49836b54688b6807
Shailesh P <129823024+spentuker@users.noreply.github.com>
2025-10-03 14:11:56 +0530

    Create 2481-StrictlyPalindromicNumber

---
+++ 2481-StrictlyPalindromicNumber/2481-StrictlyPalindromicNumber.py
@@ -0,0 +1,9 @@
+# Last updated: 10/3/2025, 2:11:55 PM
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return False
+        
