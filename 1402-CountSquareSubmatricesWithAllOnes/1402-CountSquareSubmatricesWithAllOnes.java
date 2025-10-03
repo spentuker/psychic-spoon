fba5ea4c27dc3c25685acf7d8443de6bfe6036cd
Shailesh P <129823024+spentuker@users.noreply.github.com>
2025-10-03 14:12:28 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,33 @@
+// Last updated: 10/3/2025, 2:12:28 PM
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int c=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0) continue;
+                c++;
+                int side=1;
+                while(true){
+                    if(checkOnes(matrix,i,j,side)){side++; c++;}
+                    else break;
+                    
+                }
+            }
+        }
+        return c;
+    }
+    boolean checkOnes(int matrix[][],int i,int j,int side){
+
+        int i1=side+i;int j1=side+j;
+        if(i1>=matrix.length || j1>=matrix[0].length) return false;
+        for(int k=i;k<=i1;k++){
+            for(int l=j;l<=j1;l++){
+                if(matrix[k][l]==0) return false;
+            }
+        }
+        return true;
+
+
+    }
+
+}
