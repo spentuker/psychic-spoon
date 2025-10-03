c66086a2905e81a21c241d5529c9fdc071f48ce4
Shailesh P <129823024+spentuker@users.noreply.github.com>
2025-10-03 14:14:39 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,21 @@
+// Last updated: 10/3/2025, 2:14:38 PM
+public class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+        
+        int left = 0, right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
